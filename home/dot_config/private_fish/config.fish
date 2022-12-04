812509a7514f15967fd5f96208b6370e8733e9e6
Jon Polom <jon@subly.me>
2022-12-04 15:20:34 -0500

    fixed paths in a hopefully portable way

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    if string match -q 'Darwin' -- $(uname)
+        fish_add_path -P /opt/homebrew/bin
+    end
+
+    fish_add_path -P ~/.local/bin
 end

67c304763ac657d083d5b9d0f46770f27b354c81
Jon Polom <jon@subly.me>
2023-07-24 21:03:16 -0400

    lsp-zero baseline

--- home/dot_config/nvim/lua/plugins/lsp-zero.lua
+++ home/dot_config/nvim/lua/plugins/lsp-zero.lua
@@ -19,7 +19,29 @@ return {
                     },
                 },
             },
-            { "jose-elias-alvarez/null-ls.nvim" },
+            {
+                "jose-elias-alvarez/null-ls.nvim",
+                dependencies = {
+                   "nvim-lua/plenary.nvim",
+                },
+                config = function()
+                    local null_ls = require("null-ls")
+
+                    null_ls.setup({
+                        sources = {
+                            null_ls.builtins.diagnostics.hadolint,
+                            null_ls.builtins.diagnostics.shellcheck,
+                            null_ls.builtins.formatting.black,
+                            null_ls.builtins.formatting.isort,
+                            null_ls.builtins.formatting.jq,
+                            null_ls.builtins.formatting.prettier,
+                            null_ls.builtins.formatting.shellharden,
+                            null_ls.builtins.formatting.shfmt,
+                            null_ls.builtins.formatting.stylua,
+                        },
+                    })
+                end,
+            },
             { "jayp0521/mason-null-ls.nvim" },
             -- Autocompletion
             { "hrsh7th/nvim-cmp" },
@@ -46,22 +68,61 @@ return {
 
             lsp.setup()
 
-            local null_ls = require("null-ls")
-            local null_opts = lsp.build_options("null-ls", {})
-            null_ls.setup({
-                on_attach = function(client, bufnr)
-                    null_opts.on_attach(client, bufnr)
-                end,
+            lsp.set_sign_icons({
+                error = '✘',
+                warn = '▲',
+                hint = '⚑',
+                info = ''
+            })
+
+            vim.diagnostic.config({
+                virtual_text = true,
+                severity_sort = true,
+                float = {
+                    style = 'minimal',
+                    border = 'rounded',
+                    source = 'always',
+                    header = '',
+                    prefix = '',
+                },
+            })
+
+            local cmp = require('cmp')
+            local cmp_action = require('lsp-zero.cmp').action()
+
+            require('luasnip.loaders.from_vscode').lazy_load()
+            vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+            cmp.setup({
+                preselect = 'item',
+                completion = {
+                    completeopt = 'menu,menuone,noinsert'
+                },
                 sources = {
-                    null_ls.builtins.diagnostics.hadolint,
-                    null_ls.builtins.diagnostics.shellcheck,
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-                    null_ls.builtins.formatting.jq,
-                    null_ls.builtins.formatting.prettier,
-                    null_ls.builtins.formatting.shellharden,
-                    null_ls.builtins.formatting.shfmt,
-                    null_ls.builtins.formatting.stylua,
+                    {name = 'path'},
+                    {name = 'nvim_lsp'},
+                    {name = 'nvim_lua'},
+                    {name = 'buffer', keyword_length = 3},
+                    {name = 'luasnip', keyword_length = 2},
+                },
+                mapping = {
+                    -- confirm completion item
+                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+                    -- toggle completion menu
+                    ['<C-e>'] = cmp_action.toggle_completion(),
+
+                    -- tab complete
+                    ['<Tab>'] = cmp_action.tab_complete(),
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+                    -- navigate between snippet placeholder
+                    ['<C-d>'] = cmp_action.luasnip_jump_forward(),
+                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+                    -- scroll documention window
+                    ['<C-f>'] = cmp.mapping.scroll_docs(-5),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(5),
                 },
             })
         end,

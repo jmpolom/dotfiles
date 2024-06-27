76063abb4cff153879348ba2d00bb3e06205e40e
Jon Polom <jon@subly.me>
2024-06-26 22:31:40 -0400

    fix fzf-lua

--- home/dot_config/nvim/lua/plugins/fzf-lua.lua
+++ home/dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -6,10 +6,10 @@ return {
         opts = {
             fzf_opts = {
                 ["--info"] = "default",
-                grep = {
-                    rg_opts = [[--hidden --column -g "!.git" --line-number --color=always --smart-case]],
-                },
-            }
+            },
+            grep = {
+                rg_opts = '--hidden --column -g "!.git" --line-number --color=always --smart-case',
+            },
         },
     }
 }

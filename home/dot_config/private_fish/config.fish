2d064f4bb63cdc28d5b572f230fcb2d4bfc0d7c2
Jon Polom <jon@subly.me>
2024-08-16 20:52:37 -0400

    goodbye fish

--- home/dot_config/private_fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    if string match -q 'Darwin' -- $(uname)
-        fish_add_path -P /opt/homebrew/bin
-    end
-
-    fish_add_path -P ~/.local/bin
-end
--- home/dot_config/private_fish/functions/macos_add_fish_shell.fish
+++
@@ -1,3 +0,0 @@
-function macos_add_fish_shell
-    fgrep -qx (command -v fish) /etc/shells || echo (command -v fish) | sudo tee -a /etc/shells
-end

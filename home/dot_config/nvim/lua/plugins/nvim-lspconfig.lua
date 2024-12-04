d6a095c17cf6a28e734ff6cce6d85280f7386f2b
Jon Polom <jon@subly.me>
2024-12-03 20:17:11 -0500

    update lua-language-server config for nvim api

--- home/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -19,32 +19,32 @@ return {
             lspconfig.lua_ls.setup({
                 capabilities = lsp_capabilities,
                 on_init = function(client)
-                  local path = client.workspace_folders[1].name
-                  -- TODO: vim.loop is deprecated so this needs to be fixed soon
-                  if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-                    client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                      Lua = {
-                        runtime = {
-                          -- Tell the language server which version of Lua you're using
-                          -- (most likely LuaJIT in the case of Neovim)
-                          version = 'LuaJIT'
-                        },
-                        -- Make the server aware of Neovim runtime files
-                        workspace = {
-                          checkThirdParty = false,
-                          library = {
+                    if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                      return
+                    end
+                end
+
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT'
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
                             vim.env.VIMRUNTIME
+                            -- Depending on the usage, you might want to add additional paths here.
                             -- "${3rd}/luv/library"
                             -- "${3rd}/busted/library",
-                          }
-                          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                          -- library = vim.api.nvim_get_runtime_file("", true)
                         }
-                      }
-                    })
-                    client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-                  end
-                  return true
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                })
                 end,
             })
 

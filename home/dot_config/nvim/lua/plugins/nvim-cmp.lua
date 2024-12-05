cb7c0a0ca4288e74a01d2de0d71d763f00571f1b
Jon Polom <jon@subly.me>
2024-12-04 22:16:30 -0500

    remove duplicate dep and format fix

--- home/dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ home/dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -7,7 +7,6 @@ return {
             { "hrsh7th/cmp-nvim-lsp" },
             { "hrsh7th/cmp-nvim-lua" },
             { "hrsh7th/cmp-path" },
-            { "hrsh7th/nvim-cmp" },
             { "saadparwaiz1/cmp_luasnip" },
             -- Snippets
             { "L3MON4D3/LuaSnip" },
@@ -17,39 +16,39 @@ return {
             { "ray-x/lsp_signature.nvim" },
         },
         config = function()
-            local cmp = require('cmp')
+            local cmp = require("cmp")
 
-            require('luasnip.loaders.from_vscode').lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
                 completion = {
-                    completeopt = 'menu,menuone,noinsert,noselect'
+                    completeopt = "menu,menuone,noinsert,noselect"
                 },
                 sources = {
-                    {name = 'path'},
-                    {name = 'nvim_lsp'},
-                    {name = 'nvim_lua'},
-                    {name = 'buffer', keyword_length = 3},
-                    {name = 'luasnip', keyword_length = 2},
+                    {name = "path"},
+                    {name = "nvim_lsp"},
+                    {name = "nvim_lua"},
+                    {name = "buffer", keyword_length = 3},
+                    {name = "luasnip", keyword_length = 2},
                 },
                 mapping = cmp.mapping.preset.insert({
                     -- confirm completion item
-                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+                    ["<CR>"] = cmp.mapping.confirm({select = false}),
 
                     -- toggle completion menu
-                    ['<C-S-Space>'] = cmp.mapping.complete(),
+                    ["<C-S-Space>"] = cmp.mapping.complete(),
 
                     -- move in list
-                    ['<Tab>'] = cmp.mapping.select_next_item(),
-                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
                     -- scroll documention window
-                    ['<C-f>'] = cmp.mapping.scroll_docs(-5),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(5),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(-5),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(5),
                 }),
                 snippet = {
                   expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
+                    require("luasnip").lsp_expand(args.body)
                   end,
                 },
             })

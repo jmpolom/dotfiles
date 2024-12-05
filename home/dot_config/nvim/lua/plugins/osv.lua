855818b2625189fc418e698db41d9625fa3ca658
Jon Polom <jon@subly.me>
2024-12-05 14:50:06 -0500

    no more laziness

--- home/dot_config/nvim/lua/plugins/osv.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {
-        "jbyuki/one-small-step-for-vimkind",
-        config = function()
-            vim.keymap.set("n", "<F5>", "<cmd>lua require(\"osv\").launch({port = 8086})<CR>", { noremap = true })
-        end,
-    }
-}
